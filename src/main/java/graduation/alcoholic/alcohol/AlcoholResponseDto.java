5145409d238269822259619c2ce2e41d7acbb6ab
GyeongEun Kim <kekim20@naver.com>
2022-03-19 21:37:36 +0900

    dto 수정

--- src/main/java/graduation/alcoholic/alcohol/AlcoholResponseDto.java
+++ src/main/java/graduation/alcoholic/alcohol/AlcoholResponseDto.java
@@ -1,6 +1,7 @@
 package graduation.alcoholic.alcohol;
 
 import graduation.alcoholic.domain.Alcohol;
+import graduation.alcoholic.domain.Review;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,11 +10,15 @@ public class AlcoholResponseDto {
     private Long id;
     private String name;
     private String image;
+    private Integer price;
+    private Review review;
 
     @Builder
     public AlcoholResponseDto(Alcohol a) {
         this.id=a.getId();
         this.name=a.getName();
         this.image=a.getImage();
+        this.price=a.getPrice();
+
     }
 }
