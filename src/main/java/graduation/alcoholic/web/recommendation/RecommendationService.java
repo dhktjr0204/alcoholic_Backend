8cdad2187690f3af8ed658e1d27f2d63bb0fb2b8
yevini118 <79689350+yevini118@users.noreply.github.com>
2022-03-23 18:02:24 +0900

    recommendation complete

--- src/main/java/graduation/alcoholic/web/recommendation/RecommendationService.java
+++ src/main/java/graduation/alcoholic/web/recommendation/RecommendationService.java
@@ -15,6 +15,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import static graduation.alcoholic.domain.enums.Type.증류주;
+import static java.util.Comparator.comparing;
 
 
 @RequiredArgsConstructor
@@ -55,34 +56,47 @@ public class RecommendationService {
         }
 
 
+
         List<RecommendScore> recommendScoreList = getScore(requestDto, alcoholTasteList);
 
-        recommendScoreList.removeIf(recommendScore -> (recommendScore.getScore1() > 1) || (recommendScore.getScore2() > 1) || (recommendScore.getScore3() > 1) || (recommendScore.getScore4() > 1) || (recommendScore.getScore5() > 1));
+        List<Integer> noneList = getTasteCheckList(requestDto, Taste.없음);
+        List<Integer> strongList = getTasteCheckList(requestDto, Taste.강함);
+
+
+        recommendScoreList.removeIf(recommendScore -> (recommendScore.getScore1() > 2) || (recommendScore.getScore2() > 2) || (recommendScore.getScore3() > 2) || (recommendScore.getScore4() > 2) || (recommendScore.getScore5() > 2));
 
         if (!recommendScoreList.isEmpty()) {
-            recommendScoreList.sort(Comparator.comparing(RecommendScore::getTotal_score));
 
-            for (int i=0; i<5; i++) {
-                if (i >= recommendScoreList.size())
-                    break;
-                else {
-                    RecommendScore recommendScore = recommendScoreList.get(i);
-                    Alcohol alcohol = alcoholRepository.findById(recommendScore.getId()).orElseThrow();
-                    alcoholList.add(new AlcoholResponseDto(alcohol));
-                }
-            }
-            printScoreList(recommendScoreList);
+                countNoneScore(recommendScoreList, noneList);
+                countStrongScore(recommendScoreList, strongList);
+
+                recommendScoreList.sort(comparing(RecommendScore::getTotal_score).thenComparing(RecommendScore::getStd).thenComparing(comparing(RecommendScore::getNone_score).reversed()).thenComparing(comparing(RecommendScore::getStrong_score).reversed()));
+
         }
+
+        for (int i=0; i<5; i++) {
+            if (i >= recommendScoreList.size())
+                break;
+            else {
+                RecommendScore recommendScore = recommendScoreList.get(i);
+                Alcohol alcohol = alcoholRepository.findById(recommendScore.getId()).orElseThrow();
+                alcoholList.add(new AlcoholResponseDto(alcohol));
+            }
+        }
+        printScoreList(recommendScoreList);
 
         return alcoholList;
     }
 
 
     public void printScoreList(List<RecommendScore> recommendScoreList) {
+
         for (RecommendScore recommendScore : recommendScoreList) {
-            System.out.println(recommendScore.getId() + " " +
-                    recommendScore.getScore1() + " " + recommendScore.getScore2() + " " + recommendScore.getScore3() + " " + recommendScore.getScore4() + " " + recommendScore.getScore5() + " " +
-                    recommendScore.getTotal_score());
+
+            System.out.printf("%-3s %d %d %d %d %d   %d %f %d %d%n",recommendScore.getId(),
+                    recommendScore.getScore1(),recommendScore.getScore2() ,recommendScore.getScore3(), recommendScore.getScore4(), recommendScore.getScore5(),
+                    recommendScore.getTotal_score(), recommendScore.getStd() ,recommendScore.getNone_score(), recommendScore.getStrong_score());
+            System.out.println();
 
         }
     }
@@ -135,6 +149,85 @@ public class RecommendationService {
         }
 
         return recommendScoreList;
+    }
+
+    public List<Integer> getTasteCheckList(RecommendRequestDto requestDto, Taste taste) {
+
+        List<Integer> checkList = new ArrayList<>();
+
+        if (requestDto.getTaste1() == Taste.없음)
+            checkList.add(1);
+        else if (requestDto.getTaste2() == Taste.없음)
+            checkList.add(2);
+        else if (requestDto.getTaste3() == Taste.없음)
+            checkList.add(3);
+        else if (requestDto.getTaste4() == Taste.없음)
+            checkList.add(4);
+        else if (requestDto.getTaste5() == Taste.없음)
+            checkList.add(5);
+
+        return checkList;
+    }
+
+    public void countNoneScore(List<RecommendScore> recommendScoreList, List<Integer> noneList) {
+
+        for (Integer none: noneList) {
+            for (RecommendScore recommendScore : recommendScoreList) {
+                switch (none) {
+                    case 1:
+                        if (recommendScore.getScore1() == 0)
+                            recommendScore.setNone_score(recommendScore.getNone_score() + 1);
+                        break;
+                    case 2:
+                        if (recommendScore.getScore2() == 0)
+                            recommendScore.setNone_score(recommendScore.getNone_score() + 1);
+                        break;
+                    case 3:
+                        if (recommendScore.getScore3() == 0)
+                            recommendScore.setNone_score(recommendScore.getNone_score() + 1);
+                        break;
+                    case 4:
+                        if (recommendScore.getScore4() == 0)
+                            recommendScore.setNone_score(recommendScore.getNone_score() + 1);
+                        break;
+                    case 5:
+                        if (recommendScore.getScore5() == 0)
+                            recommendScore.setNone_score(recommendScore.getNone_score() + 1);
+                        break;
+                }
+            }
+        }
+    }
+
+
+    public void countStrongScore(List<RecommendScore> recommendScoreList, List<Integer> strongList) {
+
+        for (Integer none: strongList) {
+            for (RecommendScore recommendScore : recommendScoreList) {
+                switch (none) {
+                    case 1:
+                        if (recommendScore.getScore1() == 0)
+                            recommendScore.setStrong_score(recommendScore.getNone_score() + 1);
+                        break;
+                    case 2:
+                        if (recommendScore.getScore2() == 0)
+                            recommendScore.setStrong_score(recommendScore.getNone_score() + 1);
+                        break;
+                    case 3:
+                        if (recommendScore.getScore3() == 0)
+                            recommendScore.setStrong_score(recommendScore.getNone_score() + 1);
+                        break;
+                    case 4:
+                        if (recommendScore.getScore4() == 0)
+                            recommendScore.setStrong_score(recommendScore.getNone_score() + 1);
+                        break;
+                    case 5:
+                        if (recommendScore.getScore5() == 0)
+                            recommendScore.setStrong_score(recommendScore.getNone_score() + 1);
+                        break;
+                }
+            }
+        }
     }
 
 }
