e8bbe2cad61669d643667fe14c6b88e02d1214d5
LAPTOP-C6ITH8JV\정지원 <dhktjr0204442@gmail.com>
2022-03-25 20:26:41 +0900

    bar update

--- src/main/java/graduation/alcoholic/web/bar/BarController.java
+++ src/main/java/graduation/alcoholic/web/bar/BarController.java
@@ -15,50 +15,49 @@ import java.util.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/bar")
 public class BarController {
     private final BarService barService;
 
     //모든 정보 가져오기
-    @GetMapping("/")
+    @GetMapping("/bar")
     public Page<BarResponseDto> getBar(@PageableDefault(size = 10) Pageable pageable){
         return barService.getBars(pageable);
     }
 
     //추가하기
-    @PostMapping("/")
+    @PostMapping("/bar")
     public ResponseEntity<Map<String,Long>> saveBar(@RequestPart("barSaveDto") BarSaveRequestDto barSaveDTO, @RequestPart(value = "fileList", required = false) List<MultipartFile> fileList){
         return barService.saveBar(barSaveDTO, fileList);
     }
 
     //상세페이지
-    @GetMapping("/{id}")
+    @GetMapping("/bar/{id}")
     public List<BarResponseDto> getBarDetail (@PathVariable Long id) {
         return barService.getBarDetail(id);
     }
 
 
     //업데이트
-    @PutMapping("/{id}")
+    @PutMapping("/bar/{id}")
     public ResponseEntity<Map<String, Boolean>> updateBar(@PathVariable Long id, @RequestPart("barUpdateDto") BarUpdateRequestDto requestDto, @RequestPart(value = "fileList", required = false) List<MultipartFile> fileList){
         return barService.updateBar(id, requestDto, fileList);
     }
 
     //삭제하기
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/bar/{id}")
     public ResponseEntity<Map<String, Boolean>> deleteBar(@PathVariable Long id){
         return barService.deleteBar(id);
     }
 
 
     //검색기능
-    @GetMapping("/search")
+    @GetMapping("/bar/search")
     public Optional<Page<BarResponseDto>> searchByTitle(@RequestParam("title") String title, @PageableDefault(size = 10) Pageable pageable){
         return Optional.of(barService.searchByTitle(title,pageable));
     }
 
     //지역별 찾기
-    @GetMapping("/search")
+    @GetMapping("/bar/search")
     public Optional<Page<BarResponseDto>> searchByLocation(@RequestParam("location") String location, @PageableDefault(size = 10) Pageable pageable){
         return Optional.of(barService.searchByLocation(location,pageable));
     }
